a746315c9a8c90c514af55a8725f616a0aae6d85
Vré <darren.de.vre@ing.com>
2023-10-19 15:22:34 +0200

    further work

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-console.log('hello');
+console.log('server starting....');
 const dgram = require('node:dgram');
-const net = require('node:net');
 const server = dgram.createSocket('udp4');
+
+var lastPacketID = 0;
 
 server.on('error', (err) => {
   console.error(`server error:\n${err.stack}`);
@@ -9,17 +10,75 @@ server.on('error', (err) => {
 });
 
 server.on('message', (msg, sender) => {
+
+  var receivedPacket = JSON.parse(msg);
+
+  var clientdatetime = receivedPacket.clientDatetime;
+
   var datetime = (new Date()).getTime();
-  var clientToServerPing = datetime - parseInt(msg);
+  var clientToServerPing = datetime - clientdatetime;
 
-  var jason = {
+  if(receivedPacket.packetID == 1){ //allows multiple tests without restarting server
+    lastPacketID = 0;
+  }
+
+  console.log("packetID: " + receivedPacket.packetID);
+  console.log("lastPacketID: " + lastPacketID);
+
+  if(receivedPacket.packetID != lastPacketID + 1){
+
+
+
+
+    if(receivedPacket.packetID > lastPacketID + 1){
+      console.log("packet went missing");
+
+      var json = {
+        "type":"errorPacket",
+        "errorType": "missingPacket",
+        "packetID":receivedPacket.packetID,
+        "clientToServerPing":clientToServerPing,
+        "serverDatetime":datetime};
+    
+      server.send(JSON.stringify(json), sender.port, sender.address, (err) => {
+        console.log(`Message sent to ${sender.address}:${sender.port}`)
+      })
+
+      lastPacketID = receivedPacket.packetID;
+    }
+
+
+
+    else if(receivedPacket.packetID < lastPacketID + 1){
+      console.log("delayed packet received");
+      console.log("packetID: " + receivedPacket.packetID)
+      
+      var json = {
+        "type":"errorPacket",
+        "errorType": "delayedPacket",
+        "packetID":receivedPacket.packetID};
+    
+      server.send(JSON.stringify(json), sender.port, sender.address, (err) => {
+        console.log(`Message sent to ${sender.address}:${sender.port}`)
+      })
+    }
+
+
+
+    return;
+  }
+
+  lastPacketID = receivedPacket.packetID;
+
+
+
+  var json = {
+    "type":"serverToClientPingResponse",
+    "packetID":receivedPacket.packetID,
     "clientToServerPing":clientToServerPing,
-    "serverdatetime":datetime};
-  // console.log(`clientToServerPing: ${clientToServerPing}`);
+    "serverDatetime":datetime};
 
-  // console.log(`datetime: ${datetime}`);
-  // console.log(`server got: ${msg} from ${sender.address}:${sender.port}`);
-  server.send(JSON.stringify(jason), sender.port, sender.address, (err) => {
+  server.send(JSON.stringify(json), sender.port, sender.address, (err) => {
     console.log(`Message sent to ${sender.address}:${sender.port}`)
   })
 });
@@ -31,6 +90,21 @@ server.on('listening', () => {
 
 server.bind(41234);
 //Prints: server listening 0.0.0.0:41234
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // const server = net.createServer((c) => {
--- network-benchmark-client/Program.cs
+++ network-benchmark-client/Program.cs
@@ -1,47 +1,161 @@
 ﻿
-
-
-using System.Diagnostics;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using System.Text.Json;
 using System.Text.Json.Nodes;
 
+
+
+
+
+
+int packetCount = 0;
+int lastPacketID = 0;
+int packetsReceived = 0;
 UdpClient client = new UdpClient(3000);
-try
+
+
+Start();
+
+
+return;
+
+
+
+
+
+
+
+void Start()
 {
+    Console.WriteLine("How many packets?");
+    packetCount = int.Parse(Console.ReadLine() ?? string.Empty);
+
     
-    client.Connect("127.0.0.1", 41234);
+    try
+    {
     
+        client.Connect("127.0.0.1", 41234);
+    
+        Thread networkListenThread = new Thread(NetworkListenWorker);
+        networkListenThread.Start();
+    
+    
+        Thread networkSendThread = new Thread(NetworkSendWorker);
+        networkSendThread.Start();
+    
+    
+        networkListenThread.Join();
+        networkSendThread.Join();
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.ToString());
+    }
+}
+
+
+void NetworkSendWorker()
+{
+    for(int i = 0; i < packetCount; i++)
+    {
+        SendPacket(i+1);
+        Thread.Sleep(1000);
+    }
+
+}
+
+
+
+
+void SendPacket(int packetNumber)
+{
     TimeSpan t = DateTime.UtcNow - new DateTime(1970, 1, 1);
     long secondsSinceEpoch = (long)t.TotalMilliseconds;
 
+    JsonNode jsonNode = new JsonObject();
+    jsonNode["packetID"] = packetNumber;
+    jsonNode["clientDatetime"] = secondsSinceEpoch;
     
-    byte[] sendBytes = Encoding.ASCII.GetBytes(secondsSinceEpoch.ToString());
     
-
+    byte[] sendBytes = Encoding.ASCII.GetBytes(jsonNode.ToString());
     
     client.Send(sendBytes, sendBytes.Length);
-    
-    
-    IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
-
-    byte[] receiveBytes = client.Receive(ref remoteEndPoint);
-    string receivedString = Encoding.ASCII.GetString(receiveBytes);
-
-    t = DateTime.UtcNow - new DateTime(1970, 1, 1);
-    secondsSinceEpoch = (long)t.TotalMilliseconds; 
-    
-    var poep = JsonNode.Parse(receivedString);
-    long serverToClient = secondsSinceEpoch - long.Parse(poep["serverdatetime"].ToString());
-    
-    Console.WriteLine("Client to server ping: " + poep["clientToServerPing"]);
-    Console.WriteLine("Server to Client ping: " + serverToClient );
-    
-    //Console.WriteLine("Message received from the server \n " + receivedString);
 }
-catch (Exception e)
+
+
+
+
+
+void NetworkListenWorker()
 {
-    Console.WriteLine(e.ToString());
+    bool done = false;
+    IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+    while (!done)
+    {
+        byte[] receiveBytes = client.Receive(ref remoteEndPoint);
+        
+        if(receiveBytes.Length == 0)
+        {
+            continue;
+        }
+        
+        packetsReceived++;
+        
+        string receivedString = Encoding.ASCII.GetString(receiveBytes);
+        
+        HandleReceivedPacket(receivedString);
+        
+        
+        if(packetsReceived == packetCount)
+        {
+            done = true;
+        }
+    }
+}
+
+void HandleReceivedPacket(string receivedPacket)
+{
+    TimeSpan t = DateTime.UtcNow - new DateTime(1970, 1, 1);
+    long secondsSinceEpoch = (long)t.TotalMilliseconds; 
+    
+    JsonNode? receivedJson = JsonNode.Parse(receivedPacket);
+    
+    
+    if(receivedJson?["type"]?.ToString() == "errorPacket")
+    {
+        
+        Console.WriteLine(receivedJson?["errorType"]?.ToString());
+        
+        switch (receivedJson?["errorType"]?.ToString())
+        {
+            case "missingPacket":
+                Console.WriteLine("A packet was lost!");
+
+                //packetsReceived += int.Parse(receivedJson?["packetID"]?.ToString()) - lastPacketID; 
+                Console.WriteLine(int.Parse(receivedJson?["packetID"]?.ToString() ?? string.Empty) - lastPacketID);
+                break;
+            case "delayedPacket":
+                Console.WriteLine("A packet was delayed!");
+                return;
+        }
+    }
+    
+    if(int.Parse(receivedJson?["packetID"]?.ToString() ?? string.Empty) != lastPacketID + 1)
+    {
+        Console.WriteLine("A packet was lost!");
+       // Console.WriteLine(int.Parse(receivedJson?["packetID"]?.ToString() ?? string.Empty) - lastPacketID);
+    }
+    
+    
+    lastPacketID = int.Parse(receivedJson?["packetID"]?.ToString() ?? string.Empty);
+    
+    long serverToClient = secondsSinceEpoch - long.Parse(receivedJson?["serverDatetime"]?.ToString() ?? string.Empty);
+        
+    long averagePing = (long.Parse(receivedJson?["clientToServerPing"]?.ToString() ?? string.Empty) + serverToClient) / 2;
+    
+        
+    Console.WriteLine("Received Packet!");
+    Console.WriteLine($"PacketID: {receivedJson?["packetID"]}, average ping: {averagePing}, C2S: {receivedJson?["clientToServerPing"]}, S2C: {serverToClient}");
+    
 }
